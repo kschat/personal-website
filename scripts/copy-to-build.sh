a70d1bf6278dc67d94f53b0657a7b6d40b4c7f78
kschat <kyleschattler@gmail.com>
2020-05-20 18:40:49 -0400

    Fix copy-to-build script

--- scripts/copy-to-build.sh
+++ scripts/copy-to-build.sh
@@ -3,23 +3,26 @@
 set -eo pipefail
 
 font_awesome_src_dir=node_modules/@fortawesome/fontawesome-free
-font_awesome_to_dir=lib/client/scss/vendor/font-awesome/
+font_awesome_scss_to_dir=lib/client/scss/vendor/font-awesome
+font_awesome_font_to_dir=dist/client/fonts/font-awesome
 
 if [ ! -d "$font_awesome_src_dir" ]; then
   echo 'FontAwesome not installed, run `yarn install` and try again' >&2
   exit 1
 fi
 
-if [ ! -d "$font_awesome_to_dir" ]; then
-  mkdir -p lib/client/scss/vendor/font-awesome/
+if [ ! -d "$font_awesome_scss_to_dir" ]; then
+  mkdir -p "$font_awesome_scss_to_dir"
   cp \
     "$font_awesome_src_dir"/scss/* \
-    lib/client/scss/vendor/font-awesome/
+    "$font_awesome_scss_to_dir"
+fi
 
-  mkdir -p dist/client/fonts/font-awesome/
+if [ ! -d "$font_awesome_font_to_dir" ]; then
+  mkdir -p "$font_awesome_font_to_dir"
   cp \
     "$font_awesome_src_dir"/webfonts/* \
-    dist/client/fonts/font-awesome/
+    "$font_awesome_font_to_dir"
 fi
 
 copyfiles \
