1955f69512897b13fca9a28e995bee1f18cc46e5
kschat <kyleschattler@gmail.com>
2020-05-20 17:56:49 -0400

    Fix build and watch scripts

--- scripts/build-scss.sh
+++ scripts/build-scss.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-font_awesome_dir=node_modules/@fortawesome/fontawesome-free
+font_awesome_src_dir=node_modules/@fortawesome/fontawesome-free
+font_awesome_to_dir=lib/client/scss/vendor/font-awesome/
 
-if [ ! -d "$font_awesome_dir" ]; then
+if [ ! -d "$font_awesome_src_dir" ]; then
   echo 'FontAwesome not installed, run `yarn install` and try again' >&2
   exit 1
 fi
 
-mkdir -p lib/client/scss/vendor/font-awesome/
-cp \
-  "$font_awesome_dir"/scss/* \
-  lib/client/scss/vendor/font-awesome/
-
+if [ ! -d "$font_awesome_to_dir" ]; then
+  mkdir -p lib/client/scss/vendor/font-awesome/
+  cp \
+    "$font_awesome_src_dir"/scss/* \
+    lib/client/scss/vendor/font-awesome/
+fi
 
   # --output-style compressed \
 node-sass \
--- scripts/copy-to-build.sh
+++ scripts/copy-to-build.sh
@@ -2,22 +2,25 @@
 
 set -eo pipefail
 
-font_awesome_dir=node_modules/@fortawesome/fontawesome-free
+font_awesome_src_dir=node_modules/@fortawesome/fontawesome-free
+font_awesome_to_dir=lib/client/scss/vendor/font-awesome/
 
-if [ ! -d "$font_awesome_dir" ]; then
+if [ ! -d "$font_awesome_src_dir" ]; then
   echo 'FontAwesome not installed, run `yarn install` and try again' >&2
   exit 1
 fi
 
-mkdir -p lib/client/scss/vendor/font-awesome/
-cp \
-  "$font_awesome_dir"/scss/* \
-  lib/client/scss/vendor/font-awesome/
+if [ ! -d "$font_awesome_to_dir" ]; then
+  mkdir -p lib/client/scss/vendor/font-awesome/
+  cp \
+    "$font_awesome_src_dir"/scss/* \
+    lib/client/scss/vendor/font-awesome/
 
-mkdir -p dist/client/fonts/font-awesome/
-cp \
-  "$font_awesome_dir"/webfonts/* \
-  dist/client/fonts/font-awesome/
+  mkdir -p dist/client/fonts/font-awesome/
+  cp \
+    "$font_awesome_src_dir"/webfonts/* \
+    dist/client/fonts/font-awesome/
+fi
 
 copyfiles \
   --up 1 \
