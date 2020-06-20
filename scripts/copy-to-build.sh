0e1de91c2ab3c11304add5699256207816fa4fc5
kschat <kyleschattler@gmail.com>
2020-06-20 14:09:17 -0400

    Install normalize.css from yarn instead of copy-paste into repo

--- scripts/copy-to-build.sh
+++ scripts/copy-to-build.sh
@@ -25,6 +25,21 @@ if [ ! -d "$font_awesome_font_to_dir" ]; then
     "$font_awesome_font_to_dir"
 fi
 
+normalize_css_src_dir=node_modules/normalize.css
+normalize_css_to_dir=lib/client/scss/vendor/normalize-css
+
+if [ ! -d "$normalize_css_src_dir" ]; then
+  echo 'Normalize.css not installed, run `yarn install` and try again' >&2
+  exit 1
+fi
+
+if [ ! -d "$normalize_css_to_dir" ]; then
+  mkdir -p "$normalize_css_to_dir"
+  cp \
+    "$normalize_css_src_dir"/* \
+    "$normalize_css_to_dir"
+fi
+
 copyfiles \
   --up 1 \
   'lib/client/scss/**/*.scss' \
